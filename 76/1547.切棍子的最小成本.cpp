4c6e3e39666e6bdea43ebda9c610da16726946d0
1iuy4n <2827277816@qq.com>
2024-11-15 00:10:50 +0800

    区间dp

---
+++ 76/1547.切棍子的最小成本.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <utility>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        
+    }
+};
+
+
+int main(){
+    
+    
+    
+    return 0;
+}
