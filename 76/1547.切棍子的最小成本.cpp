3edf100073b8534d6d2258adabab756db6bdd674
1iuy4n <2827277816@qq.com>
2024-11-18 17:39:54 +0800

    四个参数改两个参数

--- 76/1547.切棍子的最小成本.cpp
+++ 76/1547.切棍子的最小成本.cpp
@@ -12,10 +12,114 @@
 using namespace std;
 
 
+class Solution0 {
+public:
+    static const int MAX_N = 101;
+    int dp[MAX_N][MAX_N];
+    void build(){
+        for(int i=0; i<MAX_N; i++){
+            for(int j=0; j<MAX_N; j++){
+                dp[i][j] = -1;
+            }
+        }
+    }
+    int f(int l, int r, int cutsL, int cutsR, vector<int>& cuts){
+        if(cutsL==cutsR){
+            return 0;
+        }
+        if(dp[cutsL][cutsR]==-1){
+            int ans = INT_MAX;
+            for(int m = cutsL; m<cutsR; m++){
+                ans = min(ans, r-l+f(l, cuts[m], cutsL, m, cuts)+f(cuts[m], r, m+1, cutsR, cuts));
+            }
+            dp[cutsL][cutsR] = ans;
+        }
+        return dp[cutsL][cutsR];
+    }
+    int minCost(int n, vector<int>& cuts) {
+        build();
+        sort(cuts.begin(), cuts.end(), less<>());
+        int ans = INT_MAX;
+        for(int l=0, r=n, m=0; m <cuts.size(); m++){
+            ans = min(ans, n+f(0, cuts[m], 0, m, cuts)+f(cuts[m], n, m+1, cuts.size(), cuts));
+        }
+        return ans;
+    }
+};
+
+
+class Solution1 {
+public:
+    static const int MAX_N = 105;
+    int dp[MAX_N][MAX_N];
+    void build(){
+        for(int i=0; i<MAX_N; i++){
+            for(int j=0; j<MAX_N; j++){
+                dp[i][j] = -1;
+            }
+        }
+    }
+    int f(int cutsL, int cutsR, vector<int>& cuts){
+        if(cutsL==cutsR){
+            return 0;
+        }
+        if(dp[cutsL][cutsR]==-1){
+            int ans = INT_MAX;
+            for(int m = cutsL; m<cutsR; m++){
+                ans = min(ans, f(cutsL, m, cuts)+f(m+1, cutsR, cuts));
+            }
+            dp[cutsL][cutsR] = ans+cuts[cutsR]-cuts[cutsL-1];
+        }
+        return dp[cutsL][cutsR];
+    }
+    int minCost(int n, vector<int>& cuts) {
+        build();
+        sort(cuts.begin(), cuts.end(), less<>());
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
+        int ans = INT_MAX;
+        for(int l=0, r=n, m=1; m <cuts.size()-1; m++){
+            ans = min(ans, f(1, m, cuts)+f(m+1, cuts.size()-1, cuts));
+        }
+        return ans+n;
+    }
+};
+
+
 class Solution {
 public:
+    static const int MAX_N = 105;
+    int dp[MAX_N][MAX_N];
+    void build(){
+        for(int i=0; i<MAX_N; i++){
+            for(int j=0; j<MAX_N; j++){
+                dp[i][j] = -1;
+            }
+        }
+    }
+    int f(int cutsL, int cutsR, vector<int>& cuts){
+        if(cutsL==cutsR){
+            return 0;
+        }
+        if(dp[cutsL][cutsR]==-1){
+            int ans = INT_MAX;
+            for(int m = cutsL; m<cutsR; m++){
+                ans = min(ans, f(cutsL, m, cuts)+f(m+1, cutsR, cuts));
+            }
+            dp[cutsL][cutsR] = ans+cuts[cutsR]-cuts[cutsL-1];
+        }
+        return dp[cutsL][cutsR];
+    }
     int minCost(int n, vector<int>& cuts) {
-        
+        build();
+        sort(cuts.begin(), cuts.end(), less<>());
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
+        int ans = INT_MAX;
+        for(int l=0, r=n, m=1; m <cuts.size()-1; m++){
+            ans = min(ans, f(1, m, cuts)+f(m+1, cuts.size()-1, cuts));
+        }
+        return ans+n;
     }
 };
 
