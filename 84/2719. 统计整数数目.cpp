ddcb1f83fb56fe8031496ec36cbcb63fba4a8624
1iuy4n <2827277816@qq.com>
2025-06-26 18:09:38 +0800

    feat(84): 优化数字求和算法
    - 重构 getSum 函数，使用动态规划方法提高计算效率
    - 新增 build 函数初始化动态规划数组
    - 优化 check 函数，准确判断数字和是否在范围内
    - 主函数中添加测试用例，验证算法正确性

--- 84/2719. 统计整数数目.cpp
+++ 84/2719. 统计整数数目.cpp
@@ -14,31 +14,97 @@ using namespace std;
 class Solution {
 public:
     int mod = 1e9+7;
-    int getSum(string& num, int min_sum, int max_sum, int alreadySelect, int freeSelect, int index, int currSum){
-        if(index==0){
+    static const int MAXN = 24, MAXM = 401;
+    int dp[MAXN][2][2][MAXM];
+    void build(){
+        for(int i=0; i<MAXN; i++){
+            for(int j=0; j<MAXM; j++){
+                dp[i][0][0][j] = -1;
+                dp[i][0][1][j] = -1;
+                dp[i][1][0][j] = -1;
+                dp[i][1][1][j] = -1;
+            }
+        }
+    }
+    int getSum(vector<int>& num, int min_sum, int max_sum, int alreadySelect, int freeSelect, int index, int currSum){
+        if((currSum>max_sum) || (currSum+(num.size()-index)*9<min_sum)){
+            return 0;
+        }
+        if(index == num.size()){
             return alreadySelect;
         }
-        int ans = 0;
-        if(freeSelect){
-
+        if(dp[index][alreadySelect][freeSelect][currSum] == -1){
+            int ans = 0;
+            if(alreadySelect==0){
+                if(freeSelect==1){
+                    for(int i=0; i<=9; i++){
+                        ans = (ans + getSum(num, min_sum, max_sum, 1, 1, index+1, currSum+i))%mod;
+                    }
+                } else{
+                    ans = (ans + getSum(num, min_sum, max_sum, 0, 1, index+1, currSum))%mod;
+                    for(int i=1; i<num[index]; i++){
+                        ans = (ans + getSum(num, min_sum, max_sum, 1, 1, index+1, currSum+i))%mod;
+                    }
+                    ans = (ans + getSum(num, min_sum, max_sum, 1, 0, index+1, currSum+num[index]))%mod;
+                }
+            } else{
+                if(freeSelect==1){
+                    for(int i=0; i<=9; i++){
+                        ans = (ans + getSum(num, min_sum, max_sum, 1, 1, index+1, currSum+i))%mod;
+                    }
+                } else{
+                    for(int i=0; i<num[index]; i++){
+                        ans = (ans + getSum(num, min_sum, max_sum, 1, 1, index+1, currSum+i))%mod;
+                    }
+                    ans = (ans + getSum(num, min_sum, max_sum, 1, 0, index+1, currSum+num[index]))%mod;
+                }
+            }
+            dp[index][alreadySelect][freeSelect][currSum] = ans;
         }
-        return 0;
+        return dp[index][alreadySelect][freeSelect][currSum];
     }
-    bool check(string& num1, int min_sum, int max_sum){
-        return true;
+    bool check(vector<int>& num1, int min_sum, int max_sum){
+        int num1Sum = 0;
+        for(int i=0; i<num1.size(); i++){
+            num1Sum += num1[i];
+        }
+        return num1Sum>=min_sum && num1Sum<=max_sum;
     }
     int count(string num1, string num2, int min_sum, int max_sum) {
-        int ans = getSum(num2, min_sum, max_sum, 0, 0, 0, 0)-getSum(num1, min_sum, max_sum, 0, 0, 0, 0);
-        if(check(num1, min_sum, max_sum)){
-            ans += 1;
+        // preprocess[0] = 10;
+        // for(int i=1; i<MAXN; i++){
+        //     preprocess[i] = (10*preprocess[i-1])%mod;
+        // }
+        vector<int> int1(num1.size(), 0);
+        vector<int> int2(num2.size(), 0);
+        for(int i=0; i<num1.size(); i++){
+            int1[i] = num1[i]-'0';
+        }
+        for(int i=0; i<num2.size(); i++){
+            int2[i] = num2[i]-'0';
+        }
+        build();
+        int result2 = getSum(int2, min_sum, max_sum, 0, 0, 0, 0);
+        // cout<<result2<<endl;
+        build();
+        int result1 = getSum(int1, min_sum, max_sum, 0, 0, 0, 0);
+        // cout<<result1<<endl;
+        int ans = (result2-result1+mod)%mod;
+        if(check(int1, min_sum, max_sum)){
+            ans = (ans+1)%mod;
         }
         return ans;
     }
 };
 
-
 int main(){
-    
+
+    Solution test;
+    string num1 = "7748704426";//4179205230
+    string num2 = "7748704426";
+    int min_sum = 8;
+    int max_sum = 46;
+    cout<<test.count(num1, num2, min_sum, max_sum);
     
     
     return 0;
